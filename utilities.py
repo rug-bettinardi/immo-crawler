20deb8f7f187d26f8a0c05dbf08ff5e0270e2661
Ruggero BETTINARDI <ruggero.bettinardi@bioserenity.com>
2021-11-18 12:23:11 +0100

    updated immo utilities.py: mergeNewToOldScrapedData()

--- utilities.py
+++ utilities.py
@@ -1,18 +1,23 @@
 import os
+import ntpath
 from utils.tools import loadJson, saveJson, jprint
 
 def removeDuplicateDictsInList(lstDicts):
+    """ taken from https://stackoverflow.com/a/9428041 """
 
-    return [i for n, i in enumerate(lstDicts) if i not in lstDicts[n + 1:]]  # taken from https://stackoverflow.com/a/9428041
-    # return {frozenset(item.items()): item for item in lstDicts}.values()  # taken from: https://stackoverflow.com/a/23358757
+    return [i for n, i in enumerate(lstDicts) if i not in lstDicts[n + 1:]]
 
-def mergeNewToOldScrapedData(newJson, oldJson, tgtJson=None):
+def mergeNewToOldScrapedData(newJson, oldJson, tgtJson=None, moveNewJsonTo=None):
     """
+    Merge new scraped data with the already scraped one,
+    remove duplicate observations,
+    store them, and move the new observation to another location (to be deleted)
 
     Args:
         newJson: str, full path of new JSON file
         oldJson: str, full path of old JSON file
         tgtJson: optional. str, full path of new JSON file to create by merging oldJson + newJson
+        moveNewJsonTo: optional, str. full path to directory where to move the newJson file (and its .log, if exists)
 
     Returns:
         merged: list of dicts, oldJson + newJson
@@ -21,30 +26,28 @@ def mergeNewToOldScrapedData(newJson, oldJson, tgtJson=None):
 
     new = loadJson(src=newJson)
     old = loadJson(src=oldJson)
-    merged = old + new
-
-
+    merged = removeDuplicateDictsInList(lstDicts=old + new)
 
     if tgtJson:
         saveJson(d=merged, tgt=tgtJson)
+
+    if moveNewJsonTo:
+        os.replace(newJson, os.path.join(moveNewJsonTo, ntpath.basename(newJson)))
+
+        newLog = newJson.replace('.json', '.log')
+        if os.path.isfile(newLog):
+            os.replace(newLog, os.path.join(moveNewJsonTo, ntpath.basename(newLog)))
 
     return merged
 
 
 if __name__ == "main":
 
-    merged = mergeNewToOldScrapedData(
+    mergedDict = mergeNewToOldScrapedData(
         newJson=r"C:\CodeRug\immo\crawler\crawler\pap.json",
-        oldJson=r"C:\CodeRug\immo\crawler\crawler\pap_old.json",
-        tgtJson=r"C:\CodeRug\immo\data\rawDB.json"
+        oldJson=r"C:\CodeRug\immo\data\rawDB.json",
+        tgtJson=r"C:\CodeRug\immo\data\rawDB.json",
+        moveNewJsonTo=r"C:\CodeRug\immo\data\oldDataToDelete"
     )
 
 
-
-    merged = mergeNewToOldScrapedData(
-        newJson=r"C:\CodeRug\immo\crawler\crawler\pap.json",
-        oldJson=r"C:\CodeRug\immo\crawler\crawler\pap_old.json",
-        tgtJson=None
-    )
-
-    rd = removeDuplicateDictsInList(lstDicts=merged)
